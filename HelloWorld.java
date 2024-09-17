4b1b62f9a8ae7bd7fdd993802c33a07180dd87c6
xiaohao <15871690513@163.com>
2024-09-17 17:19:11 +0800

    test git stash

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,5 +7,7 @@ public class HelloWorld {
         System.out.println("update second...");
         System.out.println("test git pull --rebase...");
         System.out.println("random update...");
+        System.out.println("test git stash 暂存区...");
+        System.out.println("test git stash 工作区...");
     }
 }
